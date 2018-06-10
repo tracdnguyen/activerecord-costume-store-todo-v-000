f0ed3780fd2461705f832b87c8ee5d2542b48481
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-10 18:55:58 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,5 +8,5 @@
 # In this change method, create columns with the correct names and
 # value types according to the spec
 
-class CreateCostumes < ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[5.1]
 end
