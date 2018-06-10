e28de115941f77c4edb7a9dddfe72528ae53d881
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-10 19:54:06 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,5 @@
 # Create your haunted_houses migration here
-class CreateHauntedHouses
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
