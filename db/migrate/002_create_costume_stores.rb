3231cf42f8a1ae5829ce955778438e717598bfeb
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-10 19:24:47 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,6 +2,14 @@
 
 class CostumeStores
   def change
-    create_table :costume_stores
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees 
+      t.boolean :still_in_busines
+      t.timestamp :opening_time
+      t.timestamp :closing_time
+    end
   end
 end
