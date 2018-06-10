0b7dfbc9cff9eff45888705675b14c10ace2384c
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-10 19:41:23 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,7 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :still_in_busines
+      t.string :still_in_busines
       t.datetime :opening_time
       t.datetime :closing_time
     end
